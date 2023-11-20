eeca63315077fbcebbff77cb783640fa1008c79f
duks500 <duks500@gmail.com>
2023-11-19 20:07:34 -0600

    #2

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -5,13 +5,40 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
+const Wrapper = styled.div`
+  height: 43px;
+  max-width: min-content;
+  border-radius: 8px;
+  background-color: ${COLORS.transparentGray15};
+  border-style: none;
+  padding: 8px;
+  appearance: none;
+  display: flex;
+
+  &:hover {
+    color: ${COLORS.black}
+  }
+`
+
+const SelectStyled = styled.select`
+  appearance: none;
+  background-color: transparent;
+  border-style: none;
+  height: 100%;
+`
+
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <SelectStyled value={value} onChange={onChange}>
+        {children}
+      </SelectStyled>
+      <Icon
+        id="chevron-down"
+      />
+    </Wrapper>
   );
 };
 
